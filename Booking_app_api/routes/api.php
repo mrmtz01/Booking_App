59634246f7eeecb146a0d3ea40af6947564d2fc9
mrmtz01 <128842431+mrmtz01@users.noreply.github.com>
2023-09-13 09:02:27 -0600

    Add files via upload

--- Booking_app_api/routes/api.php
+++ Booking_app_api/routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\ControllerBooking;
-use App\Http\Controllers\ControllerGuest;
-use App\Http\Controllers\ControllerRoom;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControllerBooking;
+use App\Http\Controllers\ControllerRoom;
+use App\Http\Controllers\ControllerGuest;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,14 +21,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route:: get('/guests', [ControllerGuest:: class, 'index']);
-Route::post('/guests',[ControllerGuest::class, 'store']);
-Route::put('/guests/{id}',[ControllerGuest::class, 'update']);
+Route::get('/guests', [ControllerGuest::class, 'index'])->name('guest.index');
+Route::post('/guests',[ControllerGuest::class, 'store'])->name('guest.post');
+Route::put('/guests/{id}',[ControllerGuest::class, 'update'])->name('guest.put');
 
-Route:: get('/rooms', [ControllerRoom:: class, 'index']);
-Route::post('/rooms',[ControllerRoom::class, 'store']);
-Route::put('/rooms/{id}',[ControllerRoom::class, 'update']);
+Route::get('/rooms', [ControllerRoom::class, 'index'])->name('rooms.index');
+Route::post('/rooms',[ControllerRoom::class, 'store'])->name('rooms.post');
+Route::put('/rooms/{id}',[ControllerRoom::class, 'update'])->name('rooms.put');
+Route::delete('/room/{id}', [ControllerRoom::class, 'updateStatus'])->name('rooms.update');
 
-Route:: get('/bookings', [ControllerBooking:: class, 'index']);
-Route::post('/bookings',[ControllerBooking::class, 'store']);
-Route::put('/bookings/{id}',[ControllerBooking::class, 'update']);
+Route::get('/bookings', [ControllerBooking::class, 'index'])->name('bookings.index');
+Route::post('/bookings',[ControllerBooking::class, 'store'])->name('booking.post');
+Route::put('/bookings/{id}',[ControllerBooking::class, 'update'])->name('booking.put');
+Route::delete('/booking/{id}', [ControllerBooking::class, 'updateStatus'])->name('booking.update');
